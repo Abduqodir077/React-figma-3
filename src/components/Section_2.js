3d9e99b7caae2d8b0c1931b045c50ae53ade350f
Abduqodir077 <urinovabduqodir07@gmail.com>
2022-09-29 00:09:11 +0500

    Section_2 is end

--- src/components/Section_2.js
+++ src/components/Section_2.js
@@ -1,6 +1,7 @@
 import sec2_1 from "../img/sec2_1.png";
 import sec2_2 from "../img/sec2_2.png";
 import sec2_3 from "../img/sec2_3.png";
+import sec2_4 from "../img/Frame 20.png";
 
 function Section_2() {
   let data = [
@@ -39,6 +40,65 @@ function Section_2() {
               </div>
             </div>
           ))}
+        </div>
+        <div className="sec2_div2">
+          <div className="sec2_left">
+            <div className="sec2_l_p">
+            <p>Строительный отделочный <br /> инструмент</p>
+            </div>
+            <div className="sec2_l_sp">
+              <span>Перейти в каталог</span>
+              <button>
+              <img src={sec2_4} alt="" />
+              </button>
+            </div>
+          </div>
+          <div className="sec2_right">
+            <div className="sec2_r_1">
+              <div className="sec2_2_p">
+                <p>Эмали</p>
+              </div>
+              <div className="sec2_2_sp">
+                <span>Перейти в каталог</span>
+                <button>
+              <img src={sec2_4} alt="" />
+              </button>
+              </div>
+            </div>
+            <div className="sec2_r_2" id="d2">
+            <div className="sec2_3_p">
+                <p>Пены, клеи и <br /> герметики</p>
+              </div>
+              <div className="sec2_3_sp">
+                <span>Перейти в каталог</span>
+                <button>
+              <img src={sec2_4} alt="" />
+              </button>
+              </div>
+            </div>
+            <div className="sec2_r_2" id="d1">
+            <div className="sec2_3_p">
+                <p>Лакокрасочные <br /> материалы</p>
+              </div>
+              <div className="sec2_3_sp">
+                <span>Перейти в каталог</span>
+                <button>
+              <img src={sec2_4} alt="" />
+              </button>
+              </div>
+            </div>
+            <div className="sec2_r_1" id="d3">
+              <div className="sec2_2_p">
+                <p>Розетки и выключатели</p>
+              </div>
+              <div className="sec2_2_sp">
+                <span>Перейти в каталог</span>
+                <button>
+              <img src={sec2_4} alt="" />
+              </button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
